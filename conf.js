2c6479b8497bab20a4e8ee49ecd90d1b67b172e9
Bang Nguyen <bangnv77@gmail.com>
2016-11-01 15:37:36 -0700

    add multiple browserNames functionality

--- conf.js
+++ conf.js
@@ -3,7 +3,9 @@ exports.config = {
   framework: 'jasmine',
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['spec.js'],
-  capabilities: {
+  multiCapabilities: [{
     browserName: 'firefox'
-  }
+  }, {
+    browserName: 'chrome'
+  }]
 }
